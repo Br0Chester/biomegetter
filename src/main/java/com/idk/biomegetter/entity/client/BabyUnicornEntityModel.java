c0fdd578a825715ee14ca8ad2c8e78da84fde70b
Br0Chester <139231019+Br0Chester@users.noreply.github.com>
2026-08-02 20:58:36 +0300

    Baby unicorn added

--- src/main/java/com/idk/biomegetter/entity/client/BabyUnicornEntityModel.java
+++ src/main/java/com/idk/biomegetter/entity/client/BabyUnicornEntityModel.java
@@ -1,88 +1,22 @@
 package com.idk.biomegetter.entity.client;
 
-import com.idk.biomegetter.entity.animation.Unicorn_Animation;
-import com.idk.biomegetter.entity.state.UnicornEntityRendererState;
-import net.minecraft.client.animation.KeyframeAnimation;
-import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
 
 
-public class BabyUnicornEntityModel extends EntityModel<UnicornEntityRendererState> {
-
-    private final ModelPart F_leg_L;
-    private final ModelPart f_l_l_1;
-    private final ModelPart f_l_l_2;
-    private final ModelPart f_l_l_3;
-    private final ModelPart F_leg_R;
-    private final ModelPart f_l_r_1;
-    private final ModelPart f_l_r_2;
-    private final ModelPart f_l_r_3;
-    private final ModelPart Body;
-    private final ModelPart Tail;
-    private final ModelPart tail_1;
-    private final ModelPart tail_2;
-    private final ModelPart tail_3;
-    private final ModelPart tail_brush;
-    private final ModelPart B_leg_R;
-    private final ModelPart b_l_r_1;
-    private final ModelPart b_l_r_2;
-    private final ModelPart b_l_r_3;
-    private final ModelPart B_leg_L;
-    private final ModelPart b_l_l_1;
-    private final ModelPart b_l_l_2;
-    private final ModelPart b_l_l_3;
-    private final ModelPart neck_and_head;
-    private final ModelPart neck_1;
-    private final ModelPart neck_2;
-    private final ModelPart head;
-    private final ModelPart mouth;
-    private final ModelPart ears;
-
-    //  Видимо, эти переменные мы обязаны тут объявлять
-    private final KeyframeAnimation walk;
+public class BabyUnicornEntityModel extends UnicornEntityModel {
 
     public BabyUnicornEntityModel(ModelPart root) {
         super(root);
-        this.F_leg_L = root.getChild("F_leg_L");
-        this.f_l_l_1 = F_leg_L.getChild("f_l_l_1");
-        this.f_l_l_2 = f_l_l_1.getChild("f_l_l_2");
-        this.f_l_l_3 = f_l_l_2.getChild("f_l_l_3");
-        this.F_leg_R = root.getChild("F_leg_R");
-        this.f_l_r_1 = F_leg_R.getChild("f_l_r_1");
-        this.f_l_r_2 = f_l_r_1.getChild("f_l_r_2");
-        this.f_l_r_3 = f_l_r_2.getChild("f_l_r_3");
-        this.Body = root.getChild("Body");
-        this.Tail = Body.getChild("Tail");
-        this.tail_1 = Tail.getChild("tail_1");
-        this.tail_2 = tail_1.getChild("tail_2");
-        this.tail_3 = tail_2.getChild("tail_3");
-        this.tail_brush = tail_3.getChild("tail_brush");
-        this.B_leg_R = root.getChild("B_leg_R");
-        this.b_l_r_1 = B_leg_R.getChild("b_l_r_1");
-        this.b_l_r_2 = b_l_r_1.getChild("b_l_r_2");
-        this.b_l_r_3 = b_l_r_2.getChild("b_l_r_3");
-        this.B_leg_L = root.getChild("B_leg_L");
-        this.b_l_l_1 = B_leg_L.getChild("b_l_l_1");
-        this.b_l_l_2 = b_l_l_1.getChild("b_l_l_2");
-        this.b_l_l_3 = b_l_l_2.getChild("b_l_l_3");
-        this.neck_and_head = root.getChild("neck_and_head");
-        this.neck_1 = neck_and_head.getChild("neck_1");
-        this.neck_2 = neck_1.getChild("neck_2");
-        this.head = neck_2.getChild("head");
-        this.mouth = head.getChild("mouth");
-        this.ears = head.getChild("ears");
-
-//      Видимо, эти переменные мы обязаны тут объявлять
-        this.walk = Unicorn_Animation.WALK.bake(root);
+//        this.babyWalk = BabyUnicorn_Animation.WALK.bake(root); // своя анимация, свой файл-константа
     }
 
     public static LayerDefinition getTexturedModelData() {
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
 
-        PartDefinition F_leg_L = partdefinition.addOrReplaceChild("F_leg_L", CubeListBuilder.create(), PartPose.offset(30.0F, 10.0F, -4.5F));
+        PartDefinition F_leg_L = partdefinition.addOrReplaceChild("F_leg_L", CubeListBuilder.create(), PartPose.offset(3.0F, 10.0F, -4.5F));
 
         PartDefinition f_l_l_1 = F_leg_L.addOrReplaceChild("f_l_l_1", CubeListBuilder.create().texOffs(14, 27).addBox(-1.0F, -2.0F, -1.5F, 3.0F, 6.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(-1.0F, 2.0F, 0.0F));
 
@@ -178,12 +112,12 @@ public class BabyUnicornEntityModel extends EntityModel<UnicornEntityRendererSta
         return LayerDefinition.create(meshdefinition, 64, 64);
     }
 
-    @Override
-    public void setupAnim(UnicornEntityRendererState state) {
-        super.setupAnim(state);
-
-        if (state.walkAnimationState.isStarted()) {
-            this.walk.apply(state.walkAnimationState, state.ageInTicks);
-        }
-    }
+//    @Override
+//    public void setupAnim(UnicornEntityRendererState state) {
+//        super.setupAnim(state);
+//
+//        if (state.walkAnimationState.isStarted()) {
+//            this.walk.apply(state.walkAnimationState, state.ageInTicks);
+//        }
+//    }
 }
